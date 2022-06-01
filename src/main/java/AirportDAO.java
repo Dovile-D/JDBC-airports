4450be92cd4a07d19716643c2acd7f4a8f83025a
Dovile-D <dovile.d.git@gmail.com>
2022-06-01 22:00:36 +0300

    add global variables

--- src/main/java/AirportDAO.java
+++ src/main/java/AirportDAO.java
@@ -3,14 +3,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class AirportDAO {
+//    global variables:
+    private  static  final String DATABASE_NAME = "airports";
+    private static  final String DATABASE_USERNAME = "root";
+    private static final String DATABASE_PASSWORD = "";
+
     public static void create(Airport airport) {
         // new entry creation query
-        String query = "";
+        String query = "INSERT into sb_airports (biz_id, biz_name, address, city) VALUES ('Karmelavos oro uostas', " +
+                "'Karmelava, Kauno r', 'Kaunas');";
         // connect to database:
-        String url = "jdbc:mysql://localhost:3306/" + "airports";
+        String url = "jdbc:mysql://localhost:3306/" + DATABASE_NAME;
         try{
             // create connection to a database:
-            Connection connection = DriverManager.getConnection(url, "root","");
+            Connection connection = DriverManager.getConnection(url, DATABASE_USERNAME,DATABASE_PASSWORD);
             System.out.println("Connection successful! ");
         } catch (SQLException e) {
             System.out.println("Connection failed! Read more: ");
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-
+//       create:
 
     Airport airport01 = new Airport("Paris de Gaulle", "Roissy-en-France", "Paris" );
     AirportDAO.create(airport01);
+
+    Airport airport02 = new Airport("Malaga Airport", "Av. del Comandante García Morato, s/n, 29004",
+            "Malaga" );
+    AirportDAO.create(airport01);
+
+    AirportDAO.create(airport02);
+
+//    update:
+
     }
 }
